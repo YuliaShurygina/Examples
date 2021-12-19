878ceb7f24d069419cefcfccd78b586e662b0af2
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-20 01:04:02 +0300

    dog and friends added

---
+++ example008_DogFriends/Program.cs
@@ -0,0 +1,28 @@
+﻿
+//Console.WriteLine("Введите расстояние ");
+int distance = 10000;
+//Console.WriteLine("Введите скорость первого друга ");
+int firstFriendSpeed = 1;
+//Console.WriteLine("Введите скорость второго друга ");
+int secondFriendSpeed = 2;
+//Console.WriteLine("Введите скорость собаки ");
+int dogSpeed = 5;
+int friend = 2;
+int count = 0;
+while (distance>10)
+{
+    if(friend == 1)
+    {
+        double time = distance / (dogSpeed+firstFriendSpeed);
+        friend = 2;
+    }
+    else
+    {
+        double time = distance / (dogSpeed+secondFriendSpeed);
+        friend = 1;
+    }
+    distance = distance - time*(firstFriendSpeed+secondFriendSpeed);
+    count = count + 1;
+    
+}
+Console.WriteLine(count);
